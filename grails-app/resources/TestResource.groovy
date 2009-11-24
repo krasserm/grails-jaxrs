222ba2288e46f9e0c295fcfe16a664fe3c184c36
Martin Krasser <krasserm@googlemail.com>
2009-11-24 15:19:04 +0000

    remove accidentially committed test classes

--- grails-app/resources/TestResource.groovy
+++
@@ -1,27 +0,0 @@
-import javax.ws.rs.Consumesimport javax.ws.rs.POSTimport grails.converters.XMLimport javax.ws.rs.GET
-import javax.ws.rs.Path
-import javax.ws.rs.Produces
-
-import grails.converters.JSON
-
-@Path('/test')
-class TestResource {
-
-//    @GET 
-//    @Produces(['application/json', 'text/x-json'])
-//    JSON getTestJson() { 
-//        new Person(name:'martin') as JSON
-//    }
-    
-//    @POST//    @Consumes(['text/x-json', 'text/xml'])//    @Produces('text/x-json')//    JSON getTestJson(Map params) {//        println params//        def p = new Person(params)//        p.name = p.name + 'xxx'//        p as JSON//    }        @GET 
-    @Produces(['application/xml', 'application/json'])
-    Object getTestXml() { 
-        new Person(name:'martin')
-    }
-    
-//    @GET @Produces('*/*')
-//    JSON getTestDefault() { 
-//        getTestJson()
-//    }
-      
-}
