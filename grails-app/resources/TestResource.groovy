b7a106a41195d79aad05c4237146f34319fc29c6
Martin Krasser <krasserm@googlemail.com>
2009-12-06 09:51:13 +0000

    revert accidential commit of local test settings

--- grails-app/resources/TestResource.groovy
+++
@@ -1,10 +0,0 @@
-import javax.ws.rs.Consumesimport javax.ws.rs.POSTimport javax.ws.rs.GET
-import javax.ws.rs.Path
-import javax.ws.rs.Produces
-
-import grails.converters.JSON
-import grails.converters.XMLimport org.grails.jaxrs.provider.DomainObjectNotFoundException
-@Path('/api/test')
-class TestResource {
-
-    @GET     @Produces(['application/xml'])    Person getTest() {        return new Person(age:33, name:'mike')        //throw new DomainObjectNotFoundException(String.class, 1)    }}
