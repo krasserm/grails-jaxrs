78cf02988d9ce725f63433c63698a916c4cbb95e
Stephane Prohaszka <stephane.prohaszka@gmail.com>
2011-11-07 18:19:09 +0100

    Upgrade for compatibility with Grails 2.0.0.RC1

--- JaxrsGrailsPlugin.groovy
+++ JaxrsGrailsPlugin.groovy
@@ -17,9 +17,9 @@ import org.grails.jaxrs.web.JaxrsListener
 
 class JaxrsGrailsPlugin {
     // the plugin version
-    def version = "0.5"
+    def version = "0.6"
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "1.2.0 > *"
+    def grailsVersion = "2.0 > *"
     // the other plugins this plugin depends on
     def dependsOn = [:]
     // resources that are excluded from plugin packaging
--- src/groovy/org/grails/jaxrs/support/RequestStreamAdapter.groovy
+++ src/groovy/org/grails/jaxrs/support/RequestStreamAdapter.groovy
@@ -6,6 +6,14 @@ import javax.servlet.ServletInputStream
 import org.springframework.mock.web.DelegatingServletInputStream;
 import org.springframework.mock.web.MockHttpServletRequest;
 
+import javax.servlet.AsyncContext
+import javax.servlet.ServletRequest
+import javax.servlet.ServletResponse
+import javax.servlet.http.HttpServletResponse
+import javax.servlet.http.Part
+import javax.servlet.ServletException
+import javax.servlet.DispatcherType
+
 class RequestStreamAdapter extends MockHttpServletRequest {
 
     InputStream stream
@@ -26,5 +34,57 @@ class RequestStreamAdapter extends MockHttpServletRequest {
             return super.getInputStream()
         }
     }
+
+    /* The following code is taken from MockHttpServletRequest 3.1.0.RC1 */
+    //---------------------------------------------------------------------
+    // Methods introduced in Servlet 3.0
+    //---------------------------------------------------------------------
+
+    public AsyncContext getAsyncContext() {
+        throw new UnsupportedOperationException();
+    }
+
+    public DispatcherType getDispatcherType() {
+        throw new UnsupportedOperationException();
+    }
+
+    public boolean isAsyncSupported() {
+        throw new UnsupportedOperationException();
+    }
+
+    public AsyncContext startAsync() {
+        throw new UnsupportedOperationException();
+    }
+
+    public AsyncContext startAsync(ServletRequest arg0, ServletResponse arg1) {
+        throw new UnsupportedOperationException();
+    }
+
+    public boolean isAsyncStarted() {
+        throw new UnsupportedOperationException();
+    }
+
+    public boolean authenticate(HttpServletResponse arg0) throws IOException, ServletException {
+        throw new UnsupportedOperationException();
+    }
     
+    public void addPart(Part part) {
+        parts.put(part.getName(), part);
+    }
+
+    public Part getPart(String key) throws IOException, IllegalStateException, ServletException {
+        return parts.get(key);
+    }
+
+    public Collection<Part> getParts() throws IOException, IllegalStateException, ServletException {
+        return parts.values();
+    }
+
+    public void login(String arg0, String arg1) throws ServletException {
+        throw new UnsupportedOperationException();
+    }
+
+    public void logout() throws ServletException {
+        throw new UnsupportedOperationException();
+    }
 }
