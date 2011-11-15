384d923abe8f2ac96d3cd419d5f6c4a8474bd6d7
Martin Krasser <krasserm@googlemail.com>
2011-11-15 09:25:55 +0100

    Fix broken create-resource script

--- scripts/_Events.groovy
+++ scripts/_Events.groovy
@@ -1,7 +1,7 @@
 import grails.util.GrailsNameUtils// Assumptions://// - eventCreatedFile is called before eventCreatedArtefact// - instances of this script are never executed concurrently
 def createdFile
 
-eventCreatedFile = { file ->    createdFile = file}eventCreatedArtefact = { type, name ->   if (type == 'Resource') {       def resourceName = name       def resourcePath = GrailsNameUtils.getPropertyName(name)       ant.replace(file: createdFile, token: "@resource.name@", value: resourceName)       ant.replace(file: createdFile, token: "@resource.path@", value: resourcePath)   }
+eventCreatedFile = { file ->    createdFile = file}eventCreatedArtefact = { type, name ->       if (type == 'Resource' || type.toString().endsWith('Resource.groovy') /* workaround for Grails 2.0.0.RC1 */) {       def resourceName = name       def resourcePath = GrailsNameUtils.getPropertyName(name)       ant.replace(file: createdFile, token: "@resource.name@", value: resourceName)       ant.replace(file: createdFile, token: "@resource.path@", value: resourcePath)   }
    }
 
 
