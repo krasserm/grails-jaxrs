b210152597c0752fb8dbdbbc7b54ea70a70bff56
Martin Krasser <krasserm@googlemail.com>
2012-01-22 14:06:32 +0100

    Set development version to 0.7

--- JaxrsGrailsPlugin.groovy
+++ JaxrsGrailsPlugin.groovy
@@ -17,7 +17,7 @@ import org.grails.jaxrs.web.JaxrsListener
 
 class JaxrsGrailsPlugin {
     // the plugin version
-    def version = "0.6"
+    def version = "0.7"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.0 > *"
     // the other plugins this plugin depends on
