f02b83de768101d1c3e6bb39dfa2705c64b65b47
Davide Cavestro <davide.cavestro@gmail.com>
2012-08-01 14:56:06 +0200

    Added on copied files a notice with a reference to the version of springframework for where they were taken and stating that I changed them only to add the notice itself.

--- src/java/org/springframework/mock/web/MockHttpServletRequest.java
+++ src/java/org/springframework/mock/web/MockHttpServletRequest.java
@@ -1,4 +1,7 @@
 /*
+ * WARNING: THIS FILE WAS COPIED AS-IS FROM SPRINGFRAMEWORK 3.0.5 DISTRIBUTION.
+ * ACTUALLY THE FILE WAS MERELY COPIED WITHOUT ANY OTHER CHANGES THAN THIS WARNING MESSAGE.
+ * 
  * Copyright 2002-2010 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
