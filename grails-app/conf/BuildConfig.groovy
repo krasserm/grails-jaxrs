508c9a170c48f8694ec94f8e298cf6c785235d4d
noamt <noam@10ne.org>
2014-01-20 15:50:04 +0200

    Upgrade dependency versions

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -91,7 +91,7 @@ grails.project.dependency.resolution = {
     }
 
     plugins {
-        build(':release:3.0.1', ':rest-client-builder:1.0.3') {
+        build(':release:3.0.1', ':rest-client-builder:2.0.1') {
             export = false
         }
     }
